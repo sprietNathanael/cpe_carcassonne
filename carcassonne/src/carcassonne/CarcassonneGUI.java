3afcc2f69a7effb70250feac3fbd5e2a611339c8
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-10-04 10:20:42 +0200

    Mise à jour du plateau

--- carcassonne/src/carcassonne/CarcassonneGUI.java
+++ carcassonne/src/carcassonne/CarcassonneGUI.java
@@ -4,45 +4,63 @@
  * and open the template in the editor.
  */
 package carcassonne;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.util.Observable;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+
 /**
  *
  * @author nathanael
  */
-public class CarcassonneGUI extends JFrame {
+public class CarcassonneGUI extends JFrame implements java.util.Observer {
 
     private Plateau plateau;
     private JPanel carcassonneBoard;
     private JLayeredPane layeredPane;
     private Dimension dim;
-    
+
     public CarcassonneGUI(Plateau plateau) {
         this.plateau = plateau;
         layeredPane = new JLayeredPane();
         dim = new Dimension(800, 800);
         getContentPane().add(layeredPane);
         layeredPane.setPreferredSize(dim);
-        
-        carcassonneBoard =  new JPanel();
+
+        carcassonneBoard = new JPanel();
         layeredPane.add(carcassonneBoard, JLayeredPane.DEFAULT_LAYER);
-        carcassonneBoard.setLayout( new GridLayout(this.plateau.getTailleX(), this.plateau.getTailleY()) );
-        carcassonneBoard.setPreferredSize( dim );
+        carcassonneBoard.setLayout(new GridLayout(this.plateau.getTailleX(), this.plateau.getTailleY()));
+        carcassonneBoard.setPreferredSize(dim);
         carcassonneBoard.setBounds(0, 0, dim.width, dim.height);
-        
-        for (int i = 0; i < (this.plateau.getTailleX() * this.plateau.getTailleY()) ; i++)
-        {
-            JPanel square = new JPanel( new BorderLayout() );
+
+        for (int i = 0; i < (this.plateau.getTailleX() * this.plateau.getTailleY()); i++) {
+            JPanel square = new JPanel(new BorderLayout());
             square.setBorder(BorderFactory.createLineBorder(Color.black));
-            carcassonneBoard.add( square );
-            square.setBackground(Color.white );
+            JLabel jlabel;
+            jlabel = new JLabel("" + i);
+            square.add(jlabel);
+            carcassonneBoard.add(square);
+            square.setBackground(Color.white);
         }
     }
     
+    @Override
+    public void update(Observable o, Object arg) {
+        for (int incX = 0; incX < this.plateau.getTailleX(); incX++) {
+            for (int incY = 0; incY < this.plateau.getTailleY(); incY++) {
+                System.out.println(this.plateau.getElement(incX, incY));
+            }
+        }
+    }
+    
+    
+
 }
--- carcassonne/src/carcassonne/LauncherGUI.java
+++ carcassonne/src/carcassonne/LauncherGUI.java
@@ -5,23 +5,29 @@
  */
 package carcassonne;
 
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.JFrame;
 
 /**
  *
  * @author nathanael
  */
-public class LauncherGUI {
+public class LauncherGUI{
 
     public static void main(String[] args) {
 
         JFrame frame;
         Plateau plateau = new Plateau(10, 10);
         frame = new CarcassonneGUI(plateau);
+        GUIController controller = new GUIController();
+        controller.addObserver((Observer) frame);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocation(600, 10);
         frame.pack();
         frame.setVisible(true);
+        controller.notifyObservers();;
     }
 
+    
 }
