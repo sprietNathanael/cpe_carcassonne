7babf185021fa6fb5084b58c7c650d4da941272c
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-10-04 09:48:03 +0200

    Create an empty game board

--- carcassonne/src/carcassonne/CarcassonneGUI.java
+++ carcassonne/src/carcassonne/CarcassonneGUI.java
@@ -4,11 +4,45 @@
  * and open the template in the editor.
  */
 package carcassonne;
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 /**
  *
  * @author nathanael
  */
-public class CarcassonneGUI {
+public class CarcassonneGUI extends JFrame {
+
+    private Plateau plateau;
+    private JPanel carcassonneBoard;
+    private JLayeredPane layeredPane;
+    private Dimension dim;
+    
+    public CarcassonneGUI(Plateau plateau) {
+        this.plateau = plateau;
+        layeredPane = new JLayeredPane();
+        dim = new Dimension(800, 800);
+        getContentPane().add(layeredPane);
+        layeredPane.setPreferredSize(dim);
+        
+        carcassonneBoard =  new JPanel();
+        layeredPane.add(carcassonneBoard, JLayeredPane.DEFAULT_LAYER);
+        carcassonneBoard.setLayout( new GridLayout(this.plateau.getTailleX(), this.plateau.getTailleY()) );
+        carcassonneBoard.setPreferredSize( dim );
+        carcassonneBoard.setBounds(0, 0, dim.width, dim.height);
+        
+        for (int i = 0; i < (this.plateau.getTailleX() * this.plateau.getTailleY()) ; i++)
+        {
+            JPanel square = new JPanel( new BorderLayout() );
+            square.setBorder(BorderFactory.createLineBorder(Color.black));
+            carcassonneBoard.add( square );
+            square.setBackground(Color.white );
+        }
+    }
     
 }
--- carcassonne/src/carcassonne/LauncherGUI.java
+++ carcassonne/src/carcassonne/LauncherGUI.java
@@ -5,10 +5,23 @@
  */
 package carcassonne;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author nathanael
  */
 public class LauncherGUI {
-    
+
+    public static void main(String[] args) {
+
+        JFrame frame;
+        Plateau plateau = new Plateau(10, 10);
+        frame = new CarcassonneGUI(plateau);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocation(600, 10);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
