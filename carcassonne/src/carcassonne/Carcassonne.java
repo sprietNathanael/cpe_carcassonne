df25c1d7ca0bce38de3c9d59bf162c002efcb957
Unknown <thomas.cordier@cpe.fr>
2017-10-04 11:03:32 +0200

    creation de données tuiles

--- carcassonne/src/carcassonne/Carcassonne.java
+++ carcassonne/src/carcassonne/Carcassonne.java
@@ -17,7 +17,14 @@ public class Carcassonne {
     public static void main(String[] args) {
 
         Plateau p = new Plateau(10, 10);
-        p.setElement(new Tuile(2, 2, EnumTuile.route, EnumTuile.route, EnumTuile.route, EnumTuile.route));
+        p.setElement(new Tuile(2, 2, EnumTuile.route, EnumTuile.champ, EnumTuile.ville, EnumTuile.route));
+        p.setElement(new Tuile(2, 3, EnumTuile.champ, EnumTuile.route, EnumTuile.route, EnumTuile.ville));
+        p.setElement(new Tuile(5, 5, EnumTuile.champ, EnumTuile.route, EnumTuile.route, EnumTuile.ville));
+        p.setElement(new Tuile(5, 4, EnumTuile.champ, EnumTuile.route, EnumTuile.ville, EnumTuile.route));
+        p.setElement(new Tuile(4, 4, EnumTuile.route, EnumTuile.champ, EnumTuile.champ, EnumTuile.route));
+        p.setElement(new Tuile(3, 4, EnumTuile.champ, EnumTuile.route, EnumTuile.champ, EnumTuile.route));
+        p.setElement(new Tuile(2, 4, EnumTuile.route, EnumTuile.champ, EnumTuile.champ, EnumTuile.route));
+        p.setElement(new Tuile(3, 3, EnumTuile.route, EnumTuile.ville, EnumTuile.route, EnumTuile.ville));
         System.out.println(p);
 
     }
