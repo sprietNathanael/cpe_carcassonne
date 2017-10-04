2647b6c1afc037581b9897f8fe1adcbaf6f7c776
Unknown <tom-mollaret@orange.fr>
2017-10-04 11:42:48 +0200

    Bug sur la position identique de deux éléments corrigé

--- carcassonne/src/carcassonne/Carcassonne.java
+++ carcassonne/src/carcassonne/Carcassonne.java
@@ -18,16 +18,13 @@ public class Carcassonne {
 
         Plateau p = new Plateau(10, 10);
         p.setElement(new Tuile(2, 2, EnumTuile.route, EnumTuile.route, EnumTuile.route, EnumTuile.route));
-        Tuile t = new Tuile(2,3, EnumTuile.route, EnumTuile.route, EnumTuile.route, EnumTuile.route);
+        Tuile t = new Tuile(4,2, EnumTuile.route, EnumTuile.route, EnumTuile.route, EnumTuile.route);
         
         if(p.verifPos(t)==true)
         {
          p.setElement(t);
         }
         
-
-        
-        p.setElement(new Tuile(2, 2, EnumTuile.route, EnumTuile.champ, EnumTuile.ville, EnumTuile.route));
         p.setElement(new Tuile(2, 3, EnumTuile.champ, EnumTuile.route, EnumTuile.route, EnumTuile.ville));
         p.setElement(new Tuile(5, 5, EnumTuile.champ, EnumTuile.route, EnumTuile.route, EnumTuile.ville));
         p.setElement(new Tuile(5, 4, EnumTuile.champ, EnumTuile.route, EnumTuile.ville, EnumTuile.route));
