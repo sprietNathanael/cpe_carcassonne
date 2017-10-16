b6b21d6cfd110fffc35083f3c46504241f365196
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-10-16 10:20:25 +0200

    Commence l'implémentation des classes de type

---
+++ carcassonne/src/carcassonne/model/type/AbstractType.java
@@ -0,0 +1,19 @@
+/*
+ * Carcassonne Project - 2017 - 2018
+ * Created by Bertrand Challet, Thomas Cordier, Étienne Durousset, Thomas Mollaret and Nathanaël Spriet
+ * CPE 4th year project
+ */
+package carcassonne.model.type;
+
+/**
+ *
+ * @author nathanael
+ */
+public abstract class AbstractType
+{
+
+    public AbstractType()
+    {
+    }
+    
+}
