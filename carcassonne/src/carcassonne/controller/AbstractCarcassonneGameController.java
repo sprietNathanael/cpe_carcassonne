32a606272b6719849287dd4f7fb37fd07e20f92b
Unknown <thomas.cordier@cpe.fr>
2017-10-20 09:32:41 +0200

    Création des classes contrôleur et vue

---
+++ carcassonne/src/carcassonne/controller/AbstractCarcassonneGameController.java
@@ -0,0 +1,15 @@
+/*
+ * Carcassonne Project - 2017 - 2018
+ * Created by Bertrand Challet, Thomas Cordier, Étienne Durousset, Thomas Mollaret and Nathanaël Spriet
+ * CPE 4th year project
+ */
+package carcassonne.controller;
+
+/**
+ * Abstract class which represents a generic class
+ * @author Thomas
+ */
+public class AbstractCarcassonneGameController implements CarcassonneGameControllerInterface
+{
+    
+}
