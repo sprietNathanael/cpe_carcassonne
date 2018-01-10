b82916b06236bd2ae1b224540238ab59af1114f8
Spartom <tom-mollaret@orange.fr>
2018-01-10 13:09:47 +0100

    Avancement multi

--- carcassonne/src/carcassonne/controller/testMulti/Client.java
+++ carcassonne/src/carcassonne/controller/testMulti/Client.java
@@ -9,44 +9,23 @@ package carcassonne.controller.testMulti;
  *
  * @author thomas
  */
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
+import java.net.*;
 
 public class Client {
-	
-	public static void main(String[] zero) {
-		
+
+	public static void main(String[] zero){
 		
 		Socket socket;
-		BufferedReader in;
-		PrintWriter out;
-
 		try {
-		
-			socket = new Socket(InetAddress.getLocalHost(),100);	
-		        System.out.println("Demande de connexion");
-
-		        in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
-		        String message_distant = in.readLine();
-		        System.out.println(message_distant);
-		        
-		        socket.close();
-		       
-		}catch (UnknownHostException e) {
-			
-			e.printStackTrace();
-		}catch (IOException e) {
+		socket = new Socket("localhost",2009);
+		socket.close();
+		} catch (IOException e) {
 			
 			e.printStackTrace();
 		}
 	}
-
 }
+
 
 
