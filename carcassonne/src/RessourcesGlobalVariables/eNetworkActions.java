a93b72b4c21034a727d2f2661387c6acacd226fd
Unknown <thomas.cordier@cpe.fr>
2018-01-24 15:59:31 +0100

    ajout enum networkaction

---
+++ carcassonne/src/RessourcesGlobalVariables/eNetworkActions.java
@@ -0,0 +1,21 @@
+/*
+ * Carcassonne Project - 2017 - 2018
+ * Created by Bertrand Challet, Thomas Cordier, Étienne Durousset, Thomas Mollaret and Nathanaël Spriet
+ * CPE 4th year project
+ */
+package RessourcesGlobalVariables;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Thomas
+ */
+public enum eNetworkActions implements Serializable
+{
+    rotateRight,
+    putTile,
+    putMeeple,
+    beginGame,
+    endTurn
+}
